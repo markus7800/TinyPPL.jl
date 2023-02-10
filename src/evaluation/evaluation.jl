e453c9fe75915773458beb2a44e9ea49dab8e88e
Markus Boeck <markus.boeck7800@hotmail.com>
2023-02-10 12:21:03 +0100

    move proposal to distribution folder

--- src/evaluation/evaluation.jl
+++ src/evaluation/evaluation.jl
@@ -3,7 +3,7 @@ module Evaluation
     import ProgressLogging: @progress
 
     include("../utils/utils.jl")
-    include("../utils/proposal.jl")
+    include("../distributions/proposal.jl")
     include("sampler.jl")
     include("core.jl")
     include("Forward.jl")
