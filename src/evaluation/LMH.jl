91528eca4617a8c181aaa89b7e25ae536c267350
Markus Boeck <markus.boeck7800@hotmail.com>
2023-02-26 11:55:56 +0100

    gibbs single site

--- src/evaluation/LMH.jl
+++ src/evaluation/LMH.jl
@@ -1,5 +1,6 @@
 
 import ..TinyPPL.Distributions: Proposal, logpdf
+import Random
 
 mutable struct LMH <: Sampler
     W::Float64
@@ -74,9 +75,65 @@ function single_site_sampler(model::Function, args::Tuple, observations::Dict, n
     return traces, retvals, logprobs
 end
 
-function lmh(model::Function, args::Tuple, observations::Dict, n_samples::Int; proposal::Proposal=Proposal())
+function gibbs_single_site_sampler(model::Function, args::Tuple, observations::Dict, n_samples::Int, sampler::Sampler)
+    traces = Vector{Dict{Any, Real}}(undef, n_samples)
+    retvals = Vector{Any}(undef, n_samples)
+    logprobs = Vector{Float64}(undef, n_samples)
+
+    n_accepted = 0
+    retval_current = model(args..., sampler, observations)
+    W_current = sampler.W
+    Q_current = sampler.Q
+    trace_current = sampler.trace
+
+    addresses = collect(keys(trace_current))
+
+    @progress for i in 1:n_samples
+
+        Random.shuffle!(addresses)
+        for addr in addresses
+            sampler.W = 0.
+            sampler.Q = 0.
+            sampler.Q_correction = 0.
+            sampler.trace_current = trace_current
+            sampler.trace = Dict{Any, Real}()
+
+            sampler.resample_addr = addr
+
+            retval_proposed = model(args..., sampler, observations)
+            trace_proposed = sampler.trace
+            Q_proposed = sampler.Q
+            W_proposed = sampler.W
+
+            sampler.Q_correction += log(length(trace_current)) - log(length(trace_proposed))
+
+            log_α = W_proposed - Q_proposed - W_current + Q_current + sampler.Q_correction
+
+            if log(rand()) < log_α
+                retval_current, trace_current = retval_proposed, trace_proposed
+                W_current, Q_current = W_proposed, Q_proposed
+                n_accepted += 1
+            end
+        end
+
+        retvals[i] = retval_current
+        logprobs[i] = W_current
+        traces[i] = trace_current
+    end
+    @info "SingleSite $(typeof(sampler))" n_accepted/(n_samples*length(addresses))
+
+    return traces, retvals, logprobs
+end
+
+function lmh(model::Function, args::Tuple, observations::Dict, n_samples::Int; 
+    proposal::Proposal=Proposal(), gibbs::Bool=false)
+
     sampler = LMH(proposal)
-    return single_site_sampler(model, args, observations, n_samples, sampler)
+    if gibbs
+        return gibbs_single_site_sampler(model, args, observations, n_samples, sampler)
+    else
+        return single_site_sampler(model, args, observations, n_samples, sampler)
+    end
 end
 
 
